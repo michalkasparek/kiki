444b2b20c2b11cbd858d2f195edf833426a6b28c
Michal Kašpárek <michal.kasparek@gmail.com>
2023-04-14 22:19:07 +0200

    bič na, kámen úrazu, pod palbou…

--- kiki/kiki.py
+++ kiki/kiki.py
@@ -15,8 +15,8 @@ import re
 from markdown import markdown
 import tkinter as tk
 
-def kikiokno():
 
+def kikiokno():
     vzkaz_nahore = "Sem přijde text článku"
     vzkaz_dole = "Michal Kašpárek 2022-3\n\nAktualizace, návod, licence: https://github.com/michalkasparek/kiki\n\nKontakt: michal.kasparek@gmail.com"
 
@@ -33,29 +33,35 @@ def kikiokno():
     def vymaz(*args):
         if len(frame1.get(1.0, tk.END)) < 50:
             frame1.delete(1.0, tk.END)
-        
-    nahore = tk.Frame(master=window, height = 150, width = 720)
+
+    nahore = tk.Frame(master=window, height=150, width=720)
     nahore.pack(fill="both")
     nahore.pack_propagate(0)
 
     scroll1 = tk.Scrollbar(nahore)
-    scroll1.pack(side= "right", fill="y")
+    scroll1.pack(side="right", fill="y")
 
-    frame1 = tk.Text(master=nahore, wrap="word", padx=10, pady=10, yscrollcommand=scroll1.set)
-    frame1.pack(side = "left", fill = "x", expand = True)
+    frame1 = tk.Text(
+        master=nahore, wrap="word", padx=10, pady=10, yscrollcommand=scroll1.set
+    )
+    frame1.pack(side="left", fill="x", expand=True)
     frame1.insert(tk.END, vzkaz_nahore)
 
-    frame2 = tk.Button(master=window, text="Kiki, koukni na to", border=3, command=do_prace, pady = 3)
-    frame2.pack(fill = "both", expand = False)
+    frame2 = tk.Button(
+        master=window, text="Kiki, koukni na to", border=3, command=do_prace, pady=3
+    )
+    frame2.pack(fill="both", expand=False)
 
-    dole = tk.Frame(master=window, height = 300, width = 720)
-    dole.pack(fill="both", expand = True)
+    dole = tk.Frame(master=window, height=300, width=720)
+    dole.pack(fill="both", expand=True)
 
     scroll2 = tk.Scrollbar(dole)
-    scroll2.pack(side= "right", fill="y")
+    scroll2.pack(side="right", fill="y")
 
-    frame3 = tk.Text(master=dole, wrap="word", padx=10, pady=10, yscrollcommand=scroll2.set)
-    frame3.pack(side = "left", fill = "both", expand = True)
+    frame3 = tk.Text(
+        master=dole, wrap="word", padx=10, pady=10, yscrollcommand=scroll2.set
+    )
+    frame3.pack(side="left", fill="both", expand=True)
     frame3.insert(tk.END, vzkaz_dole)
 
     scroll1.config(command=frame1.yview)
@@ -66,58 +72,89 @@ def kikiokno():
 
     window.mainloop()
 
-def rozsekej_po_vetach(k_rozsekani):
 
+def rozsekej_po_vetach(k_rozsekani):
     neni_konec_vety = "(?<!(.Ing\.|[MJ]UDr\.|MVDr\.|.MgA\.|RNDr\.|PhDr\.|rmDr\.|prof\.|.Mgr\.|..Bc\.|..Dr.|.tzv\.|..tj\.|zejm\.|\szvl\.))"
 
     vety = re.sub("[„“]", "", k_rozsekani)
     vety = re.sub("\s+\n", "\n", vety)
-    vety = re.sub("(\w)\n", "\\1.\n", vety) # doplní tečku na konec řádků končíčích písmenem/číslem
-    vety = re.sub("\s\s+", " ", vety) # odstranění dvojtých mezer, mezer na koncích řádků atd.
-    vety = re.sub("(?<=[\.\!\?…])"+neni_konec_vety+"\s(?=([A-Z]|[ÁČĎÉĚÍŇÓŘŠŤÚŮÝŽ\(]))", "\n", vety)
-    vety = re.sub("(?<=[\.\!\?…]\))"+neni_konec_vety+"\s(?=([A-Z]|[ÁČĎÉĚÍŇÓŘŠŤÚŮÝŽ\(]))", "\n", vety)
-    vety = re.sub("(?<=[\.\!\?…])"+neni_konec_vety+"\s[–-•]\s", "\n", vety)
-    vety = re.sub("(?<=[a-ž][\.\!\?…])\s(?=\d+)", "\n", vety) # "odřenýma ušima. 28. listopadu jsem"
+    vety = re.sub(
+        "(\w)\n", "\\1.\n", vety
+    )  # doplní tečku na konec řádků končíčích písmenem/číslem
+    vety = re.sub(
+        "\s\s+", " ", vety
+    )  # odstranění dvojtých mezer, mezer na koncích řádků atd.
+    vety = re.sub(
+        "(?<=[\.\!\?…])" + neni_konec_vety + "\s(?=([A-Z]|[ÁČĎÉĚÍŇÓŘŠŤÚŮÝŽ\(]))",
+        "\n",
+        vety,
+    )
+    vety = re.sub(
+        "(?<=[\.\!\?…]\))" + neni_konec_vety + "\s(?=([A-Z]|[ÁČĎÉĚÍŇÓŘŠŤÚŮÝŽ\(]))",
+        "\n",
+        vety,
+    )
+    vety = re.sub("(?<=[\.\!\?…])" + neni_konec_vety + "\s[–-•]\s", "\n", vety)
+    vety = re.sub(
+        "(?<=[a-ž][\.\!\?…])\s(?=\d+)", "\n", vety
+    )  # "odřenýma ušima. 28. listopadu jsem"
     vety = vety.splitlines()
 
     return vety
 
+
 class Kiki:
-
     def __init__(self, surovy_vstup):
-
         os.chdir(os.path.dirname(__file__))
 
-		## Načtení slovníků
+        ## Načtení slovníků
 
-        with open(os.path.join("slovniky", "ptydepe.txt"), "r", encoding="utf-8") as ptydepe:
+        with open(
+            os.path.join("slovniky", "ptydepe.txt"), "r", encoding="utf-8"
+        ) as ptydepe:
             ptydepe = ptydepe.read().splitlines()
-        with open(os.path.join("slovniky", "notokboomer.txt"), "r", encoding="utf-8") as notokboomer_vstup:
+        with open(
+            os.path.join("slovniky", "notokboomer.txt"), "r", encoding="utf-8"
+        ) as notokboomer_vstup:
             notokboomer_vstup = notokboomer_vstup.read().splitlines()
             notokboomer = {}
             for line in notokboomer_vstup:
-                key = line.split(";")[0] # načte z každé řádky obsah před středníkem (regex s různými tvary spojení)
-                value = line.split(";")[1] # a obsah za středníkem (vysvětlení, proč je spojení blbě)
+                key = line.split(";")[
+                    0
+                ]  # načte z každé řádky obsah před středníkem (regex s různými tvary spojení)
+                value = line.split(";")[
+                    1
+                ]  # a obsah za středníkem (vysvětlení, proč je spojení blbě)
                 notokboomer[key] = value
-        with open(os.path.join("slovniky", "typochyby.txt"), "r", encoding="utf-8") as typochyby:
+        with open(
+            os.path.join("slovniky", "typochyby.txt"), "r", encoding="utf-8"
+        ) as typochyby:
             typochyby = typochyby.read().splitlines()
-        with open(os.path.join("slovniky", "kontextovky.txt"), "r", encoding="utf-8") as kontextovky:
+        with open(
+            os.path.join("slovniky", "kontextovky.txt"), "r", encoding="utf-8"
+        ) as kontextovky:
             kontextovky = kontextovky.read().splitlines()
-        with open(os.path.join("slovniky", "dublety.txt"), "r", encoding="utf-8") as dublety_vstup:
+        with open(
+            os.path.join("slovniky", "dublety.txt"), "r", encoding="utf-8"
+        ) as dublety_vstup:
             dublety_vstup = dublety_vstup.read().splitlines()
             dublety = []
             for line in dublety_vstup:
                 dublety.append([line.split(";")[0], line.split(";")[1]])
 
-		# Načtení uživatelských slovníků
+        # Načtení uživatelských slovníků
 
         if os.path.exists(os.path.join("slovniky", "ptydepe_pridej.txt")):
-            with open(os.path.join("slovniky", "ptydepe_pridej.txt"), "r", encoding="utf-8") as ptydepepridej:
+            with open(
+                os.path.join("slovniky", "ptydepe_pridej.txt"), "r", encoding="utf-8"
+            ) as ptydepepridej:
                 ptydepepridej = ptydepepridej.read().splitlines()
                 ptydepe = ptydepe + ptydepepridej
 
         if os.path.exists(os.path.join("slovniky", "ptydepe_odeber.txt")):
-            with open(os.path.join("slovniky", "ptydepe_odeber.txt"), "r", encoding="utf-8") as ptydepeodeber:
+            with open(
+                os.path.join("slovniky", "ptydepe_odeber.txt"), "r", encoding="utf-8"
+            ) as ptydepeodeber:
                 ptydepeodeber = ptydepeodeber.read().splitlines()
                 ptydepe = [x for x in ptydepe if x not in ptydepeodeber]
 
@@ -125,15 +162,25 @@ class Kiki:
         zajmena = "(\skter|\sjenž\s|\sjež\s|\sjehož\s|\sjemuž\s|\sjejímž\s|\sjejž\s|\sco\s|\scož\s)"
 
         self.html_vstup = markdown(surovy_vstup)
-        self.plaintext = re.sub("<(/)?br(/)?>", "\n", self.html_vstup) # nahradí tag <br> zalomením
-        self.plaintext = re.sub("<[^>]*>", "", self.plaintext) # odstraní z html zbylé tagy
+        self.plaintext = re.sub(
+            "<(/)?br(/)?>", "\n", self.html_vstup
+        )  # nahradí tag <br> zalomením
+        self.plaintext = re.sub(
+            "<[^>]*>", "", self.plaintext
+        )  # odstraní z html zbylé tagy
         self.plaintext_1radek = re.sub("\n", " ", self.plaintext)
 
-        self.titulek = self.plaintext.partition('\n')[0]
-        
-        self.slova = re.sub("(https?:\/\/\S*|www.\\S*)", "", self.plaintext_1radek) # odstraní urls
-        self.slova = re.sub("[,\.:;!?\(\)„“…'\"\']", "", self.slova) # vymaže interpunkci
-        self.slova = re.sub("(-|–|—)", " ", self.slova) # dlouhá-sousloví se nebudou počítat
+        self.titulek = self.plaintext.partition("\n")[0]
+
+        self.slova = re.sub(
+            "(https?:\/\/\S*|www.\\S*)", "", self.plaintext_1radek
+        )  # odstraní urls
+        self.slova = re.sub(
+            "[,\.:;!?\(\)„“…'\"']", "", self.slova
+        )  # vymaže interpunkci
+        self.slova = re.sub(
+            "(-|–|—)", " ", self.slova
+        )  # dlouhá-sousloví se nebudou počítat
         self.slova = self.slova.split()
         self.nejdelsi_slovo = max(self.slova, key=len)
         self.nejdelsi_slovo_delka = len(self.nejdelsi_slovo)
@@ -142,7 +189,9 @@ class Kiki:
         self.pocet_znaku = len(self.plaintext)
         self.pocet_normostran = self.pocet_znaku / 1800
         self.ns = "{:.1f}".format(self.pocet_normostran).replace(".", ",")
-        self.minuty_cteni = round(self.pocet_slov/200) # 200 slov za minutu (zdroj: internet)
+        self.minuty_cteni = round(
+            self.pocet_slov / 200
+        )  # 200 slov za minutu (zdroj: internet)
 
         self.vety = rozsekej_po_vetach(self.plaintext)
 
@@ -152,28 +201,32 @@ class Kiki:
 
         # Věta s nejvíce vztažnými zájmeny
 
-        self.vztazna_zajmena = {} 
+        self.vztazna_zajmena = {}
         for x in self.vety:
             pocet_zajmen = len(re.findall(zajmena, x, re.IGNORECASE))
             self.vztazna_zajmena[x] = pocet_zajmen
         self.nejvic_zajmen = max(self.vztazna_zajmena, key=self.vztazna_zajmena.get)
-        self.pocet_zajmen = max(self.vztazna_zajmena.values()) 
+        self.pocet_zajmen = max(self.vztazna_zajmena.values())
 
         # Věta s nejvíce interpunkčními znaménky
 
         self.carkyvevetach = {}
         for x in self.vety:
             carky = len(re.findall(interpunkce, x))
-            self.carkyvevetach[x] = carky 
+            self.carkyvevetach[x] = carky
             self.nejviccarek = max(self.carkyvevetach, key=self.carkyvevetach.get)
             self.pocetcarek = max(self.carkyvevetach.values())
 
         # Slova za uvozovkami
 
-        self.citace = re.split("[,\?\!]“[^\n]", self.plaintext) # rozdělí dokument na konci citací
+        self.citace = re.split(
+            "[,\?\!]“[^\n]", self.plaintext
+        )  # rozdělí dokument na konci citací
         del self.citace[0]
         self.slova_po_uvozovkach = [x.split()[0] for x in self.citace]
-        self.slova_po_uvozovkach = [re.sub("[\.,\?:;]", "", x) for x in self.slova_po_uvozovkach] # odstraní bordýlek
+        self.slova_po_uvozovkach = [
+            re.sub("[\.,\?:;]", "", x) for x in self.slova_po_uvozovkach
+        ]  # odstraní bordýlek
 
         # Ošklivé fráze
 
@@ -189,7 +242,9 @@ class Kiki:
         for key, value in notokboomer.items():
             boomerstina_vyskyt = re.findall(key, self.plaintext, re.IGNORECASE)
             if boomerstina_vyskyt:
-                self.boomerstiny_nalezene.append(str(", ".join(boomerstina_vyskyt)) + ": " + value)
+                self.boomerstiny_nalezene.append(
+                    str(", ".join(boomerstina_vyskyt)) + ": " + value
+                )
 
         # Dublety
 
@@ -203,11 +258,15 @@ class Kiki:
         # Jedno slovo dvakrát po sobě
 
         self.opakovani = re.findall("\\b(\\w+)\\s\\1\\b", self.plaintext, re.IGNORECASE)
-        self.opakovani = [re.sub("(\\w+)", "\\1 \\1", x) for x in self.opakovani] # zduplikuje duplikáty
+        self.opakovani = [
+            re.sub("(\\w+)", "\\1 \\1", x) for x in self.opakovani
+        ]  # zduplikuje duplikáty
 
         # Pojmy v uvozovkách
 
-        self.uvozovky = re.findall("„\\w+\\s*\\w*\\s*\\w*“", self.plaintext, re.IGNORECASE)
+        self.uvozovky = re.findall(
+            "„\\w+\\s*\\w*\\s*\\w*“", self.plaintext, re.IGNORECASE
+        )
 
         # Typografické chyby
 
@@ -221,10 +280,12 @@ class Kiki:
 
         self.kontextovky_nalezene = []
         for kontextovky in kontextovky:
-            kontextovka_vyskyt = re.findall(kontextovky, self.plaintext_1radek, re.IGNORECASE)
+            kontextovka_vyskyt = re.findall(
+                kontextovky, self.plaintext_1radek, re.IGNORECASE
+            )
             if kontextovka_vyskyt:
                 self.kontextovky_nalezene.append(kontextovka_vyskyt)
-				
+
         # Nejdelší podkapitoly
 
         self.podkapitoly = re.split("(\*\*\*|## )", surovy_vstup)
@@ -234,15 +295,18 @@ class Kiki:
             self.podkapitoly.remove("## ")
         self.podkapitoly_prumer = len(surovy_vstup) / len(self.podkapitoly)
         self.nejdelsi_podkapitola = max(self.podkapitoly, key=len)
-        self.nejdelsi_podkapitola_kolikrat = len(self.nejdelsi_podkapitola) / self.podkapitoly_prumer 
-        self.nejdelsi_podkapitola_kolikrat = "{:.2f}".format(self.nejdelsi_podkapitola_kolikrat).replace(".", ",")
+        self.nejdelsi_podkapitola_kolikrat = (
+            len(self.nejdelsi_podkapitola) / self.podkapitoly_prumer
+        )
+        self.nejdelsi_podkapitola_kolikrat = "{:.2f}".format(
+            self.nejdelsi_podkapitola_kolikrat
+        ).replace(".", ",")
         self.nejdelsi_podkapitola = re.sub("\n", " ", self.nejdelsi_podkapitola)
         self.nejdelsi_podkapitola = self.nejdelsi_podkapitola.strip()[0:48]
         self.nejdelsi_podkapitola_komplet = f"{self.nejdelsi_podkapitola}… ({self.nejdelsi_podkapitola_kolikrat}× delší než průměr)"
 
         if len(self.podkapitoly) < 3:
             self.nejdelsi_podkapitola_komplet = ""
-
 
         # Úprava seznamů pro pěkný výstup
 
@@ -280,24 +344,25 @@ Správná znaménka ke zkopírování: … „ “ ‚ ‘ ×
 {nr}Pohlídat kontext:          ▼{nr}- {nr_odr.join(self.kontextovky_nalezene)}
 """
 
+
 # A do práce!
 
+
 def kikistart():
-
-    if len(sys.argv) == 1: # spuštění bez argumentu = Kiki v okně
-
+    if len(sys.argv) == 1:  # spuštění bez argumentu = Kiki v okně
         kikiokno()
 
-    else: # spuštění s argumentem = Kiki v terminálu
-
+    else:  # spuštění s argumentem = Kiki v terminálu
         try:
             with open(sys.argv[1], mode="r", encoding="utf-8") as dokument:
-                obsah = dokument.read() 
-        except UnicodeDecodeError: 
-            with open(sys.argv[1], mode="r") as dokument: # pro txt uložené v libre docs s jiným kódováním
+                obsah = dokument.read()
+        except UnicodeDecodeError:
+            with open(
+                sys.argv[1], mode="r"
+            ) as dokument:  # pro txt uložené v libre docs s jiným kódováním
                 obsah = dokument.read()
         except FileNotFoundError:
-            print ("Kiki nenašla soubor.")
+            print("Kiki nenašla soubor.")
 
         mujclanek = Kiki(obsah)
 
